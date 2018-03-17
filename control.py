db264b885961b765bab61b2c87c17381ea4ed64e
ProLoD <r0668988@kuleuven.be>
2018-03-17 15:28:42 +0100

    made interface for control between website and raspberry pi

--- control.py
+++ control.py
@@ -1,31 +1,73 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
+import requests
 
-OUTPUT_PIN = 18
+OUTPUT_PIN_LEFT = 15
+OUTPUT_PIN_RIGHT = 18
+REQUEST_URL = "https://utev.org/Data?q=move"
+# INTERVAL_MS = 0
+FORWARD_DURATION_MS = 2000
+TURN_DURATION_MS = 500
 
-turns = 0           # how fast the motor runs
-turnAmount = 1      # how many turns the motor makes
-currentTime = -1
-loopTime = -1
+start_time = -1
+current_time = -1
 
 def setup():
-    GPIO.setup(OUTPUT_PIN, GPIO.OUT)
-    currentTime = time.now()
-    loopTime = currentTime
+    GPIO.setmode(GPIO.BMC)
+    GPIO.setup(OUTPUT_PIN_LEFT, GPIO.OUT)
+    GPIO.setup(OUTPUT_PIN_RIGHT, GPIO.OUT)
 
-def loop():
-    currentTime = time.now()
-    if ( currentTime >= (loopTime + 20) ):
-        GPIO.OUT(OUTPUT_PIN, GPIO.HIGH)
+def make_request():
+    r = requests.get(REQUEST_URL)
+    json_data = r.json()
+    commands = json_data['sequence']
+    return commands
 
-        turns = turns + turnAmount
+def drive_forward():
+    start_time = time.time()
+    current_time = time.time()
+    GPIO.output(OUTPUT_PIN_LEFT, GPIO.HIGH)
+    GPIO.output(OUTPUT_PIN_RIGHT, GPIO.HIGH)
+    while current_time <= start_time + FORWARD_DURATION_MS:
+        current_time = time.time()
+    GPIO.output(OUTPUT_PIN_LEFT, GPIO.LOW)
+    GPIO.output(OUTPUT_PIN_RIGHT, GPIO.LOW)
 
-        if (turns == 0 || turns == 255):
-            turnAmount = -turnAmount
+def turn_left():
+    start_time = time.time()
+    current_time = time.time()
+    GPIO.output(OUTPUT_PIN_RIGHT, GPIO.HIGH)
+    while current_time <= start_time + TURN_DURATION_MS:
+        current_time = time.time()
+    GPIO.output(OUTPUT_PIN_RIGHT, GPIO.LOW)
 
-        if (turns == 0):
-            time.wait(5000)
+def turn_right():
+    start_time = time.time()
+    current_time = time.time()
+    GPIO.output(OUTPUT_PIN_LEFT, GPIO.HIGH)
+    while current_time <= start_time + TURN_DURATION_MS:
+        current_time = time.time()
+    GPIO.output(OUTPUT_PIN_LEFT, GPIO.LOW)
 
-        loopTime = currentTime
-    else:
-        GPIO.OUT(OUTPUT_PIN, GPIO.LOW)
+def execute(commands):
+    for command in commands.split(','):
+        if command == "forward":
+            print("go forward")
+            drive_forward()
+        elif command == "left":
+            print("turn left")
+            turn_left()
+        elif command == "right":
+            print("turn left")
+            turn_right()
+        elif command == "stay":
+            print("stay")
+
+
+if __name__ == "__main__":
+    print("start")
+    while True:
+        commands = make_request()
+        execute(commands)
+        time.sleep(1)
+    print("end")
